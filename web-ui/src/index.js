7329bf60fb6662f6e4106792e44599cfe9547885
Aydrian Howard <aydrian@gmail.com>
2018-07-19 15:04:11 -0400

    added Stitch boilerplate code

--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
 import './index.css'
 
-const title = 'My Tiny App StAR Template'
+import { Stitch } from 'mongodb-stitch-browser-sdk'
+const APP_ID = '<STITCH APP ID>'
 
-ReactDOM.render(<div>{title}</div>, document.getElementById('app'))
+class StitchApp extends Component {
+  static propTypes = {
+    appId: PropTypes.string.isRequired
+  }
+
+  constructor(props) {
+    super(props)
+    this.appId = props.appId
+    this.client = Stitch.initializeDefaultAppClient(this.appId)
+  }
+
+  render() {
+    const title = 'My Tiny App StAR Template'
+
+    return <div>{title}</div>
+  }
+}
+
+ReactDOM.render(<StitchApp appId={APP_ID} />, document.getElementById('app'))
 
 module.hot.accept()
