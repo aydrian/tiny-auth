4cbaf95ea4f646ff098ac7d1188ce0984a4dac9c
Aydrian Howard <aydrian@gmail.com>
2018-07-20 14:38:46 -0400

    Fixed a bug and display picture if available

--- web-ui/src/UserInfo.js
+++ web-ui/src/UserInfo.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { Button, Header, List } from 'semantic-ui-react'
+import { Button, Header, List, Image } from 'semantic-ui-react'
 
 const UserInfo = ({ user, logoutUser }) => {
+  const { picture, ...data } = user.profile.data
+  const dataKeys = Object.keys(data)
   return (
     <div>
       <Header as="h1">User Info</Header>
       <p>You are logged in using {user.loggedInProviderName}.</p>
       <List>
-        {Object.keys(user.profile.data).map(key => {
+        {picture && (
+          <List.Item key="picture">
+            <Image src={picture} size="small" />
+          </List.Item>
+        )}
+        {dataKeys.map(key => {
           return (
-            user.profile[key] && (
+            data[key] && (
               <List.Item key={key}>
                 <List.Header>{key}</List.Header>
-                <List.Description>{user.profile[key]}</List.Description>
+                <List.Description>{data[key]}</List.Description>
               </List.Item>
             )
           )
